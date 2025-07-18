575907cd9df2baa2dd37d3c723ef7c623c3f1c7c
Sameer Mirza <harisbaig784@gmail.com>
2025-07-18 23:39:47 +0500

    little formating on contact us

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 
+
 const ContactUs = () => {
   
   return (
+ 
  <section id="contact" className="py-20 bg-white flex flex-col items-center px-4">
   {/* Section Title */}
   <div className="text-center mb-12">
-    <h2 className="text-4xl md:text-5xl font-bold text-black mb-4">
+    <h2 className="text-4xl md:text-5xl font-bold text-[#002D4C] mb-4">
       Contact Us Now
     </h2>
-    <p className="text-lg md:text-xl text-gray-600">
+    <p className="text-lg md:text-xl text-[#002D4C]">
       Fill out the form below and we'll be in touch soon.
     </p>
   </div>
@@ -44,30 +46,24 @@ const ContactUs = () => {
 
   {/* Appointment Section Title */}
   <div className="text-center mb-6 w-full max-w-4xl mb-8">
-    <h3 className="text-2xl md:text-3xl font-bold text-black mb-4">
+    <h3 className="text-2xl md:text-3xl font-bold text-[#002D4C] mb-4">
       Book an Appointment With Us
     </h3>
-    <p className="text-lg text-gray-600">
+    <p className="text-lg text-[#002D4C]">
       Choose a time that works best for your consultation.
     </p>
   </div>
 
   {/* Appointment Calendar */}
-  <div className="w-full max-w-4xl mb-8">
+<div className="w-full max-w-4xl mb-8">
+  <div className="responsive-iframe-wrapper">
     <iframe
       src="https://link.pipelinepro.co/widget/booking/OEJgWhHim5pDGRFigkgz"
-      style={{
-        width: '100%',
-        height: '900px',
-        border: 'none',
-        borderRadius: '12px',
-        overflow: 'hidden',
-      }}
-      scrolling="no"
-      id="OEJgWhHim5pDGRFigkgz_1751880860212"
       title="Appointment Booking"
+      scrolling="no"
     />
   </div>
+</div>
 
   <script
     src="https://link.pipelinepro.co/js/form_embed.js"
